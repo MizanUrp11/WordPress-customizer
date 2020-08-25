1bf9fa3a590042bc02d5f595f4113d94a05af6ea
MizanUrp11 <mrn1105009@gmail.com>
2020-08-25 10:26:32 +0600

    Markup Customizer Section Settings

--- wp-content/themes/Customizer-customizer/inc/customizer.php
+++ wp-content/themes/Customizer-customizer/inc/customizer.php
@@ -1,34 +1,28 @@
 <?php
-
-function cust_customizer_settings($wp_customizer){
-	$wp_customizer->add_section('cust_services',array(
-		'title'=>__('Services','customizer'),
-		'priority'=>'30'
-	));
-
-	$wp_customizer->add_setting('cust_services_heading',array(
-		'default'=>"Mission Statement",
-		'transport'=>'refresh', //postMessage
-//		'type'=>'option' //theme_mod or option
-	));
-
-	$wp_customizer->add_control('cust_services_heading_ctrl',array(
-		'label'=>__('Services Heading','customizer'),
-		'section'=>'cust_services',
-		'settings'=>'cust_services_heading',
-		'type'=>'text'
-	));
-
-	$wp_customizer->add_setting('cust_services_subheading',array(
-		'transport'=>'refresh', //postMessage
-//		'type'=>'option' //theme_mod or option
-	));
-
-	$wp_customizer->add_control('cust_services_subheading_ctrl',array(
-		'label'=>__('Services Description','customizer'),
-		'section'=>'cust_services',
-		'settings'=>'cust_services_subheading',
-		'type'=>'textarea'
-	));
+function cust_customizer_settings( $wp_customizer ) {
+    $wp_customizer->add_section( 'cust_service', array(
+        'title'    => __( 'Services', 'customizer' ),
+        'priority' => '30',
+    ) );
+    $wp_customizer->add_setting( 'cust_service_setting', array(
+        'default'   => 'Mission Statement',
+        'transport' => 'refresh',
+    ) );
+    $wp_customizer->add_control( 'cust_service_setting_ctrl', array(
+        'label'    => __( 'Services Heading', 'customizer' ),
+        'section'  => 'cust_service',
+        'settings' => 'cust_service_setting',
+        'type'     => 'text',
+    ) );
+    $wp_customizer->add_setting( 'cust_service_subheading', array(
+        'default'   => 'Subheading Here',
+        'transport' => 'refresh',
+    ) );
+    $wp_customizer->add_control( 'cust_service_subheading_ctrl', array(
+        'label'    => __( 'Services Subheading', 'customizer' ),
+        'section'  => 'cust_service',
+        'settings' => 'cust_service_subheading',
+        'type'     => 'textarea',
+    ) );
 }
-add_action('customize_register','cust_customizer_settings');
+add_action( 'customize_register', 'cust_customizer_settings' );
