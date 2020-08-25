c725ed4388db901a3003368a20d79d3159c71f37
MizanUrp11 <mrn1105009@gmail.com>
2020-08-25 11:57:36 +0600

    Icon Color Number of Row Customizer

--- wp-content/themes/Customizer-customizer/inc/customizer.php
+++ wp-content/themes/Customizer-customizer/inc/customizer.php
@@ -24,5 +24,46 @@ function cust_customizer_settings( $wp_customizer ) {
         'settings' => 'cust_service_subheading',
         'type'     => 'textarea',
     ) );
+
+    $wp_customizer->add_setting( 'cust_service_show_subheading', array(
+        'default'   => 1,
+        'transport' => 'refresh',
+    ) );
+    $wp_customizer->add_control( 'cust_service_show_subheading_ctrl', array(
+        'label'    => __( 'Show Subheading', 'customizer' ),
+        'section'  => 'cust_service',
+        'settings' => 'cust_service_show_subheading',
+        'type'     => 'checkbox',
+    ) );
+
+    $wp_customizer->add_section( 'cust_service', array(
+        'title'    => __( 'Services', 'customizer' ),
+        'priority' => '30',
+    ) );
+
+    $wp_customizer->add_setting( 'cust_icon_color', array(
+        'default'   => '#dd2d6a',
+        'transport' => 'refresh',
+    ) );
+    $wp_customizer->add_control( new WP_Customize_Color_Control( $wp_customizer, 'cust_icon_color_ctrl', array(
+        'label'    => __( 'Icon Color','customizer' ),
+        'section'  => 'cust_service',
+        'settings' => 'cust_icon_color',
+    ) ) );
+
+    $wp_customizer->add_setting('cust_num_of_services',array(
+        'default'=>4,
+        'transport'=>'refresh'
+    ));
+    $wp_customizer->add_control('cust_num_of_services_ctrl',array(
+        'label'    => __( 'Number of items','customizer' ),
+        'section'  => 'cust_service',
+        'settings' => 'cust_num_of_services',
+        'type'=>'select',
+        'choices'=>array(
+            '4'=>'3 Per Row',
+            '6'=>'2 Per Row',
+        )
+    ));
 }
 add_action( 'customize_register', 'cust_customizer_settings' );
--- wp-content/themes/Customizer-customizer/page-templates/landing-new.php
+++ wp-content/themes/Customizer-customizer/page-templates/landing-new.php
@@ -24,11 +24,19 @@
                     <h1 class="heading">
                         <?php echo esc_html( get_theme_mod( 'cust_service_setting', __( 'Our Mission Statement', 'customizer' ) ) ); ?>
                     </h1>
+
+                    <?php if(get_theme_mod( 'cust_service_show_subheading', 1 )):?>
                     <p class="subheading">
                         <?php echo esc_html(get_theme_mod( 'cust_service_subheading',__('Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia minima doloremque vitae aspernatur dignissimos repudiandae a nesciunt consectetur esse placeat?','customizer') )); ?>
                     </p>
+                    <?php endif; ?>
+
+                    <?php
+                    $cust_number_of_items = get_theme_mod( 'cust_num_of_services', 4 );
+                    ?>
+
                     <div class="row sub-section">
-                        <div class="col-md-4">
+                        <div class="col-md-<?php echo esc_attr($cust_number_of_items); ?>">
                             <div class="service">
                                 <i class="far fa-building"></i>
                                 <h2>Service Name</h2>
@@ -36,7 +44,7 @@
                                     harum ipsum necessitatibus nihil, pariatur praesentium quia voluptate.</p>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-<?php echo esc_attr($cust_number_of_items); ?>">
                             <div class="service">
                                 <i class="fas fa-chart-bar"></i>
                                 <h2>Service Name</h2>
@@ -44,7 +52,7 @@
                                     harum ipsum necessitatibus nihil, pariatur praesentium quia voluptate.</p>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-<?php echo esc_attr($cust_number_of_items); ?>">
                             <div class="service">
                                 <i class="fas fa-city"></i>
                                 <h2>Service Name</h2>
@@ -52,7 +60,7 @@
                                     harum ipsum necessitatibus nihil, pariatur praesentium quia voluptate.</p>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-<?php echo esc_attr($cust_number_of_items); ?>">
                             <div class="service">
                                 <i class="fas fa-haykal"></i>
                                 <h2>Service Name</h2>
@@ -60,7 +68,7 @@
                                     harum ipsum necessitatibus nihil, pariatur praesentium quia voluptate.</p>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-<?php echo esc_attr($cust_number_of_items); ?>">
                             <div class="service">
                                 <i class="fas fa-broadcast-tower"></i>
                                 <h2>Service Name</h2>
@@ -68,7 +76,7 @@
                                     harum ipsum necessitatibus nihil, pariatur praesentium quia voluptate.</p>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-<?php echo esc_attr($cust_number_of_items); ?>">
                             <div class="service">
                                 <i class="fas fa-bicycle"></i>
                                 <h2>Service Name</h2>
