d371bf56d36bf07e3c7c927bad83b4ce14e2e661
MizanUrp11 <mrn1105009@gmail.com>
2020-08-28 08:50:31 +0600

    Sidebar Fix in Understrap Theme

--- wp-content/themes/understrap/inc/widgets.php
+++ wp-content/themes/understrap/inc/widgets.php
@@ -109,6 +109,18 @@ if ( ! function_exists( 'understrap_widgets_init' ) ) {
 
 		register_sidebar(
 			array(
+				'name'          => __( 'Shop Sidebar', 'understrap' ),
+				'id'            => 'shop-sidebar',
+				'description'   => __( 'Shop sidebar widget area', 'understrap' ),
+				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+			)
+		);
+
+		register_sidebar(
+			array(
 				'name'          => __( 'Left Sidebar', 'understrap' ),
 				'id'            => 'left-sidebar',
 				'description'   => __( 'Left sidebar widget area', 'understrap' ),
--- wp-content/themes/understrap/sidebar-templates/sidebar-right.php
+++ wp-content/themes/understrap/sidebar-templates/sidebar-right.php
@@ -6,9 +6,15 @@
  */
 
 // Exit if accessed directly.
+
+if(is_shop()){
+	$understrap_sidebar = 'shop-sidebar';
+}else{
+	$understrap_sidebar = 'right-sidebar';
+}
 defined( 'ABSPATH' ) || exit;
 
-if ( ! is_active_sidebar( 'right-sidebar' ) ) {
+if ( ! is_active_sidebar( $understrap_sidebar ) ) {
 	return;
 }
 
@@ -21,6 +27,6 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 <?php else : ?>
 	<div class="col-md-4 widget-area" id="right-sidebar" role="complementary">
 <?php endif; ?>
-<?php dynamic_sidebar( 'right-sidebar' ); ?>
+<?php dynamic_sidebar( $understrap_sidebar ); ?>
 
 </div><!-- #right-sidebar -->
