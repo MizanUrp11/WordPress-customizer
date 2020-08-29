29bde8ed313f49b02c08ef23552402bb60a87445
MizanUrp11 <mrn1105009@gmail.com>
2020-08-29 09:14:36 +0600

    Study Links for Woocommerce

---
+++ wp-content/themes/Denver/study-links.php
@@ -0,0 +1,9 @@
+Snippets
+https://docs.woocommerce.com/documentation/plugins/woocommerce/woocommerce-codex/snippets/
+
+Visual Hook Series
+https://www.businessbloomer.com/category/woocommerce-tips/visual-hook-series/
+https://www.businessbloomer.com/woocommerce-visual-hook-guide-single-product-page/
+
+Action and Filter Hook Reference
+https://woocommerce.github.io/code-reference/hooks/hooks.html
