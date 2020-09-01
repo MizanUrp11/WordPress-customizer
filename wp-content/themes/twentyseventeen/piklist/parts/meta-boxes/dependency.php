ef61d1f6e9a06393910683136edefe15e840fe71
MizanUrp11 <mrn1105009@gmail.com>
2020-09-01 13:14:32 +0600

    And Relationship Piklist

---
+++ wp-content/themes/twentyseventeen/piklist/parts/meta-boxes/dependency.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: Dependency Field
+ * Post Type: post
+ */
+
+piklist( 'field', array(
+    'type'    => 'select',
+    'field'   => 'colors',
+    'label'   => __( 'Select a color', 'twentyseventeen' ),
+    'choices' => array(
+        'red'    => __( 'Red', 'twentyseventeen' ),
+        'green'  => __( 'Green', 'twentyseventeen' ),
+        'blue'   => __( 'Blue', 'twentyseventeen' ),
+        'others' => __( 'Others', 'twentyseventeen' ),
+    ),
+) );
+
+piklist( 'field', array(
+    'type'    => 'select',
+    'field'   => 'colors2',
+    'label'   => __( 'Select a color', 'twentyseventeen' ),
+    'choices' => array(
+        'red'    => __( 'Red', 'twentyseventeen' ),
+        'green'  => __( 'Green', 'twentyseventeen' ),
+        'blue'   => __( 'Blue', 'twentyseventeen' ),
+        'others' => __( 'Others', 'twentyseventeen' ),
+    ),
+) );
+
+piklist( 'field', array(
+    'type'       => 'colorpicker',
+    'field'      => 'colorpicker',
+    'label'      => __( 'Select Color', 'twentyseventeen' ),
+    'value'      => '#ffffff',
+    'conditions' => array(
+        array(
+            'field' => 'colors',
+            'value' => 'others',
+        ),
+        array(
+            'field' => 'colors2',
+            'value' => 'red',
+        ),
+    ),
+) );
